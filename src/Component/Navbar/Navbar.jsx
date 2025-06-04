f0b662b6f29beba46015ac715c910c4101a9b7c6
Ashiqur <ashiqur1801@gmail.com>
2025-06-04 16:16:18 +0600

    logo added

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import { SiBitcoinsv } from "react-icons/si";
+import logo from '../../assets/logo.png'
 
-const Navbar = ({freeCoin}) => {
+const Navbar = ({ freeCoin }) => {
     return (
         <div>
             <div className="navbar mb-3 px-5 lg:px-0">
                 <div className="navbar-start justify-between w-full flex-1 ">
-                    <img src="../src/assets/logo.png" alt="" />
+                    <img src={logo} alt="" />
                     <p className="flex items-center text-sm md:text-base md:px-5 py-2 px-3 bg-transparent text-black border-2 border-gray-300 gap-1 shadow-md rounded-lg lg:hidden font-semibold">
                         <span>{freeCoin}</span>
                         <span>Coin</span>
@@ -13,7 +14,7 @@ const Navbar = ({freeCoin}) => {
                     </p>
                     <div className="dropdown relative">
                         <div tabIndex={0} role="button" className=" lg:hidden">
-                            <svg  xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
                         </div>
                         <ul
                             tabIndex={0}
